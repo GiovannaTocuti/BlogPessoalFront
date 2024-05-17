991cec95e64b38f30668033409742f50f6a44d80
unknown <giovannatocuti@gmail.com>
2024-05-16 22:54:38 -0300

    Adicionado o componente perfil

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,47 +1,38 @@
-import React from 'react';
-import './App.css';
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../src/contexts/AuthContext'
 
-import Navbar from './components/navBar/NavBar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
-import Cadastro from './paginas/cadastro/Cadastro';
-import Home from './paginas/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
-import ListaTemas from './components/temas/listaTemas/ListaTemas';
-import FormularioTema from './components/temas/formularioTema/FormularioTema';
-import DeletarTema from './components/temas/deletarTema/DeletarTema';
-import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
-import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
-import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+function Footer() {
 
+  const { usuario, handleLogout } = useContext(AuthContext)
 
-function App() {
+  let footerComponent
+
+  let data = new Date().getFullYear()
+
+  if(usuario.token !== '') {
+    footerComponent = (
+      <>
+        <div className="flex justify-center bg-indigo-900 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
+            <p className='text-lg'>Acesse nossas redes sociais</p>
+            <div className='flex gap-2'>
+              <LinkedinLogo size={48} weight='bold' />
+              <InstagramLogo size={48} weight='bold' />
+              <FacebookLogo size={48} weight='bold' />
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
-    <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/temas" element={<ListaTemas />} />
-              <Route path="/cadastroTema" element={<FormularioTema />} />
-              <Route path="/editarTema/:id" element={<FormularioTema />} />
-              <Route path="/deletarTema/:id" element={<DeletarTema />} />
-              <Route path="/postagens" element={<ListaPostagens />} />
-              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
-              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
-              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
+      {footerComponent}
     </>
-  );
+  )
 }
-export default App;
+
+export default Footer
